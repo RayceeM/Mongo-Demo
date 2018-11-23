73a8ec4455bca56bfaf9ad93fde6f9f416d9d178
RayceeM <mwatelaraycee33@gmail.com>
2018-11-23 13:00:50 +0300

    app interacting with MongoDB

---
+++ test/system/emps_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class EmpsTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit emps_url
+  #
+  #   assert_selector "h1", text: "Emp"
+  # end
+end
